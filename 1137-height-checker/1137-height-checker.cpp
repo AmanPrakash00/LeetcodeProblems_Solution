c046d29cc30e04035ff1ba5abd27e968f54934ad
Aman Prakash <114851919+AmanPrakash00@users.noreply.github.com>
2023-10-23 10:38:00 +0530

    Time: 0 ms (100%), Space: 8.3 MB (59%) - LeetHub

---
+++ 1137-height-checker/1137-height-checker.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        vector<int> expected = heights;
+        sort(expected.begin(),expected.end());
+        int wrongStanding = 0;
+        for(int i=0;i<heights.size();i++){
+            if(heights[i] != expected[i]){
+                wrongStanding++;
+            }
+        }
+
+        return wrongStanding;
+    }
+};
