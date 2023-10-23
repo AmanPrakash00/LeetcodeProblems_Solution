1bd4c334d3ea9dc3f06fb801aa55a14a237e6c92
Aman Prakash <114851919+AmanPrakash00@users.noreply.github.com>
2023-10-24 00:19:03 +0530

    Time: 3 ms (43.16%), Space: 7.5 MB (58.06%) - LeetHub

--- 1635-number-of-good-pairs/1635-number-of-good-pairs.cpp
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.cpp
@@ -1,34 +1,35 @@
 class Solution {
 public:
 
-    //T.C - O(n^2)
-    // int numIdenticalPairs(vector<int>& nums) {
-    //     int count = 0;
+    // T.C - O(n^2)
+    int numIdenticalPairs(vector<int>& nums) {
+        int count = 0;
 
-    //     for(int i=0;i<nums.size()-1;i++){
-    //         for(int j = i+1;j<nums.size();j++){
-    //             if(nums[i] == nums[j]){
-    //                 count++;
-    //             }
-    //         }
-    //     }
-    //     return count;
-    // }
+        for(int i=0;i<nums.size()-1;i++){
+            for(int j = i+1;j<nums.size();j++){
+                if(nums[i] == nums[j]){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
 
 
     //optimal solution O(n)
-    int numIdenticalPairs(vector<int>& nums){
-        unordered_map<int,int> m;
+    
+    // int numIdenticalPairs(vector<int>& nums){
+    //     unordered_map<int,int> m;
 
-        for(auto i : nums){
-            m[i]++;
-        }
-        int cnt = 0;
-        for(auto i: m){
-            int n = i.second;
-            cnt += (n*(n-1))/2;
-        }
+    //     for(auto i : nums){
+    //         m[i]++;
+    //     }
+    //     int cnt = 0;
+    //     for(auto i: m){
+    //         int n = i.second;
+    //         cnt += (n*(n-1))/2;
+    //     }
 
-        return cnt;
-    }
+    //     return cnt;
+    // }
 };
