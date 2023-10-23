889c1bfcd5ee189037820e431f225278501ea870
Aman Prakash <114851919+AmanPrakash00@users.noreply.github.com>
2023-10-23 11:24:01 +0530

    Time: 2 ms (49.1%), Space: 6.5 MB (12.17%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.cpp
@@ -44,6 +44,26 @@ public:
         }
         return dp[n];
     }
+    int bottomupSO(int n){
+        if(n==0){
+            return 0;
+        }
+        if(n == 1 || n == 2){
+            return 1;
+        }
+        // vector<int> dp(n+1,-1);
+        int prev3 = 0;
+        int prev2 = 1;
+        int prev1 = 1;
+        int curr = 0;
+        for(int i=3;i<=n;i++){
+            curr = prev3 + prev2 + prev1;
+            prev3 = prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return curr;
+    }
     int tribonacci(int n) {
         //1. recursive call 
 
