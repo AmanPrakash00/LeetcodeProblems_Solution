6490e781f858b23b1b23212ac22bcaf88349dc44
Aman Prakash <114851919+AmanPrakash00@users.noreply.github.com>
2023-10-24 00:18:28 +0530

    Time: 4 ms (11.7%), Space: 7.6 MB (58.06%) - LeetHub

--- 1635-number-of-good-pairs/1635-number-of-good-pairs.cpp
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.cpp
@@ -1,15 +1,34 @@
 class Solution {
 public:
-    int numIdenticalPairs(vector<int>& nums) {
-        int count = 0;
 
-        for(int i=0;i<nums.size()-1;i++){
-            for(int j = i+1;j<nums.size();j++){
-                if(nums[i] == nums[j]){
-                    count++;
-                }
-            }
+    //T.C - O(n^2)
+    // int numIdenticalPairs(vector<int>& nums) {
+    //     int count = 0;
+
+    //     for(int i=0;i<nums.size()-1;i++){
+    //         for(int j = i+1;j<nums.size();j++){
+    //             if(nums[i] == nums[j]){
+    //                 count++;
+    //             }
+    //         }
+    //     }
+    //     return count;
+    // }
+
+
+    //optimal solution O(n)
+    int numIdenticalPairs(vector<int>& nums){
+        unordered_map<int,int> m;
+
+        for(auto i : nums){
+            m[i]++;
         }
-        return count;
+        int cnt = 0;
+        for(auto i: m){
+            int n = i.second;
+            cnt += (n*(n-1))/2;
+        }
+
+        return cnt;
     }
 };
