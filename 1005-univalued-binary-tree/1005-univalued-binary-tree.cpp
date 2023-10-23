7c468c1292f4acf08fbc54a681399f4c3214139e
Aman Prakash <114851919+AmanPrakash00@users.noreply.github.com>
2023-10-23 10:05:14 +0530

    Time: 0 ms (100%), Space: 10.3 MB (30.7%) - LeetHub

--- 1005-univalued-binary-tree/1005-univalued-binary-tree.cpp
+++ 1005-univalued-binary-tree/1005-univalued-binary-tree.cpp
@@ -11,6 +11,7 @@
  */
 class Solution {
 public:
+    // Approach -> 1
     bool recSolve(TreeNode* &root ,int target){
         if(root == NULL){
             return true;
@@ -25,11 +26,33 @@ public:
 
         return (leftAns && rightAns);
     }
-    bool isUnivalTree(TreeNode* root) {
+
+    //Approach -> 2
+    //we know that in univalued BT we have only 1 element
+    //we can do using map if (map.size>1) return false 
+    bool solveRecMap(TreeNode* &root , unordered_map<int,int> &m){
+        if(m.size()>1){
+            return false;
+        }
         if(root == NULL){
             return true;
         }
-        int target = root -> val;
-        return recSolve(root,target);
+        m[root -> val]++;
+        bool leftAns = solveRecMap(root -> left ,m);
+        bool rightAns = solveRecMap(root -> right ,m);
+
+        return (leftAns && rightAns);
+    }
+    bool isUnivalTree(TreeNode* root) {
+        // approach 1
+        // if(root == NULL){
+        //     return true;
+        // }
+        
+        // int target = root -> val;
+        // return recSolve(root,target);
+        unordered_map<int,int> m;
+
+        return solveRecMap(root ,m);
     }
 };
