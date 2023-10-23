d665688d56e14a0a819977c2d78f01bf4a8b3061
Aman Prakash <114851919+AmanPrakash00@users.noreply.github.com>
2023-10-23 11:03:01 +0530

    Time: 8 ms (7.32%), Space: 8 MB (61.3%) - LeetHub

---
+++ 1217-relative-sort-array/1217-relative-sort-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+        vector<int> ans;
+
+        for(int i = 0;i< arr2.size();i++){
+            for(int j = 0;j<arr1.size();j++){
+                if(arr2[i] == arr1[j]){
+                    ans.push_back(arr2[i]);
+                    arr1[j] = -1;
+                }
+            }
+        }
+        sort(arr1.begin(),arr1.end());
+        int index = upper_bound(arr1.begin(),arr1.end(),-1)-arr1.begin();
+        for(int i = index;i< arr1.size();i++){
+            if(arr1[i] != -1){
+                ans.push_back(arr1[i]);
+            }
+        }
+
+        return ans;
+    }
+};
