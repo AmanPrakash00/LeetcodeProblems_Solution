45eaabf70ce08800efdb5ba84b04fff6a8f9fae1
Aman Prakash <114851919+AmanPrakash00@users.noreply.github.com>
2023-10-24 05:31:21 +0530

    Time: 0 ms (100%), Space: 6.3 MB (44.67%) - LeetHub

--- 2491-smallest-even-multiple/2491-smallest-even-multiple.cpp
+++ 2491-smallest-even-multiple/2491-smallest-even-multiple.cpp
@@ -2,12 +2,10 @@ class Solution {
 public:
     int smallestEvenMultiple(int n) {
         
-        int nth = 2 * n;
-        for(int i = 1;i<=nth;i++){
-            if(i%2==0 && i%n==0){
-                return i;
-            }
+        if(n%2==0){
+            return n;
+        }else{
+            return n*2;
         }
-        return -1;
     }
 };
