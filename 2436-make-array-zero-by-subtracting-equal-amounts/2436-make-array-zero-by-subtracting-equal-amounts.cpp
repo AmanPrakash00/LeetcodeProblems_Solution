99b9503b2beabb76fa262d45a6e321124d37726d
Aman Prakash <114851919+AmanPrakash00@users.noreply.github.com>
2023-10-30 07:26:24 +0530

    Time: 0 ms (100%), Space: 8.5 MB (78.93%) - LeetHub

---
+++ 2436-make-array-zero-by-subtracting-equal-amounts/2436-make-array-zero-by-subtracting-equal-amounts.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int minifind(vector<int> &nums){
+        int mini = INT_MAX;
+        for(int i = 0;i<nums.size();i++){
+            if(nums[i] != 0){
+                mini = min(mini,nums[i]);
+            }
+        }
+        return mini;
+    }
+    int minimumOperations(vector<int>& nums) {
+        if(nums.size()==1 && nums[0] ==0){
+            return 0;
+        }
+        if(nums.size()==1 && nums[0] !=0){
+            return 1;
+        }
+        int cnt = 0;
+        int maxi = INT_MIN;
+        int index = -1;
+        for(int i = 0;i<nums.size();i++){
+            if(maxi<nums[i]){
+                maxi = nums[i];
+                index = i;
+            }
+        }
+        while(nums[index] != 0){
+            int mini = minifind(nums);
+            for(int i=0;i<nums.size();i++){
+                if(nums[i] != 0){
+                    nums[i] = nums[i] - mini;
+                }
+            }
+            cnt++;
+        }
+        return cnt;
+    }
+};
