1bb50cc6876bbe8f68de5542c28ab5bba524c983
Aman Prakash <114851919+AmanPrakash00@users.noreply.github.com>
2023-10-30 23:00:38 +0530

    Time: 2 ms (53.6%), Space: 8.7 MB (45.35%) - LeetHub

--- 2436-make-array-zero-by-subtracting-equal-amounts/2436-make-array-zero-by-subtracting-equal-amounts.cpp
+++ 2436-make-array-zero-by-subtracting-equal-amounts/2436-make-array-zero-by-subtracting-equal-amounts.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /*
     int minifind(vector<int> &nums){
         int mini = INT_MAX;
         for(int i = 0;i<nums.size();i++){
@@ -34,6 +35,32 @@ public:
             }
             cnt++;
         }
+        return cnt;
+    }
+    */
+    int minimumOperations(vector<int>& nums){
+        if(nums.size()==1 && nums[0] ==0){
+            return 0;
+        }
+        if(nums.size()==1 && nums[0] !=0){
+            return 1;
+        }
+
+        int cnt = 0;
+
+        sort(nums.begin(),nums.end());
+
+        for(int i=0;i<nums.size();i++){
+
+            if(nums[i] != 0){
+                cnt++;
+                int data = nums[i];
+                for(int j = i;j<nums.size();j++){
+                    nums[j] -= data;
+                }
+            }
+        }
+
         return cnt;
     }
 };
