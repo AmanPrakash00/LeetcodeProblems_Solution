cb6481ea46063ae77495b8598c1597de269d8317
Aman Prakash <114851919+AmanPrakash00@users.noreply.github.com>
2023-11-08 19:46:31 +0530

    Time: 0 ms (100%), Space: 8.8 MB (93.55%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,5 +1,26 @@
 class Solution {
 public:
+/* approach 1
+    void permutations(vector<int>&nums,int i,vector<vector<int>>&ans){
+        if(i>=nums.size()){
+            bool isPresent = find(ans.begin(), ans.end(), nums)!= ans.end();
+            if(isPresent){
+                return;
+            }else{
+                ans.push_back(nums);
+                return;
+            }
+        }
+
+        for(int j = i;j<nums.size();j++){
+            swap(nums[i],nums[j]);
+            permutations(nums,i+1,ans);
+            //backtrack
+            swap(nums[i],nums[j]);
+        }
+    }
+*/
+/* approach 2 using set
     void permutations(vector<int>&nums,int i,set<vector<int>>&s){
         if(i>=nums.size()){
             s.insert(nums);
@@ -12,13 +33,22 @@ public:
             swap(nums[i],nums[j]);
         }
     }
+*/
+
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        set<vector<int>> s;
+        // set<vector<int>> s;
+        // vector<vector<int>>ans;
+        // permutations(nums,0,s);
+        // for(auto i:s){
+        //     ans.push_back(i);
+        // }
+        // return ans;
+
         vector<vector<int>>ans;
-        permutations(nums,0,s);
-        for(auto i:s){
-            ans.push_back(i);
-        }
+        sort(nums.begin(),nums.end());
+        do{
+             ans.push_back(nums);
+        } while(next_permutation(nums.begin(),nums.end()));
         return ans;
     }
 };
