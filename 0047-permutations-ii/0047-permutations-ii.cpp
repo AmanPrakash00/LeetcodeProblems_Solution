884f3dd106cd51e583984843e16c91add60277ac
Aman Prakash <114851919+AmanPrakash00@users.noreply.github.com>
2023-11-08 19:38:20 +0530

    Time: 50 ms (30.74%), Space: 10.1 MB (41.11%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
-    void permutations(vector<int>&nums,int i,vector<vector<int>>&ans){
+    void permutations(vector<int>&nums,int i,set<vector<int>>&s){
         if(i>=nums.size()){
-            bool isPresent = find(ans.begin(), ans.end(), nums)!= ans.end();
-            if(isPresent){
-                return;
-            }else{
-                ans.push_back(nums);
-                return;
-            }
+            s.insert(nums);
+            return;
         }
-
         for(int j = i;j<nums.size();j++){
             swap(nums[i],nums[j]);
-            permutations(nums,i+1,ans);
+            permutations(nums,i+1,s);
             //backtrack
             swap(nums[i],nums[j]);
         }
     }
     vector<vector<int>> permuteUnique(vector<int>& nums) {
+        set<vector<int>> s;
         vector<vector<int>>ans;
-        permutations(nums,0,ans);
+        permutations(nums,0,s);
+        for(auto i:s){
+            ans.push_back(i);
+        }
         return ans;
     }
 };
