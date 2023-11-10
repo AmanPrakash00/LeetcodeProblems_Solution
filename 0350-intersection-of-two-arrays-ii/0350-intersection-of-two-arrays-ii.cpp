f9d500214f4e72f57d474fd738fcd7042fb82f1d
Aman Prakash <114851919+AmanPrakash00@users.noreply.github.com>
2023-11-10 19:23:39 +0530

    Time: 4 ms (59%), Space: 10.5 MB (53%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -18,7 +18,28 @@ public:
     }
 
 */
+    sort(nums1.begin(),nums1.end());
+    sort(nums2.begin(),nums2.end());
 
+    vector<int>res;
+    int n=nums1.size(),m=nums2.size();
+    int i=0,j=0;
+    while(i<n && j<m) {
+        if(nums1[i]==nums2[j]) {
+            res.push_back(nums1[i]);
+            i++;
+            j++;
+        }else if(nums1[i]<nums2[j]) {
+            i++;
+        }else {
+            j++;
+        }
+    } 
+    return res;
+    }
+
+
+/* both O(n) thoda aur sapce optimize ho sakta hai 
     unordered_map<int,int> m;
     for(int i=0;i<nums1.size();i++){
         m[nums1[i]]++;
@@ -35,5 +56,24 @@ public:
     }
     return ans;
 
+*/
+
+/*  Answer array nhi li phir bhi kuch space kam hua nhi hahah
+    unordered_map<int,int> m;
+    for(int i=0;i<nums1.size();i++){
+        m[nums1[i]]++;
     }
+    nums1.clear();
+    for(int i=0;i<nums2.size();i++){
+        int data = nums2[i];
+        if(m.find(data) != m.end()){
+            if(m[data] > 0){
+                nums1.push_back(data);
+                m[data]--;
+            }
+        }
+    }
+    return nums1;
+    }
+*/
 };
