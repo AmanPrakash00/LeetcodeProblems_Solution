4c7d75191d2141569744b44621b7d86705c33f58
Aman Prakash <114851919+AmanPrakash00@users.noreply.github.com>
2023-11-10 19:17:53 +0530

    Time: 3 ms (83.47%), Space: 10.8 MB (39.2%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -1,25 +1,9 @@
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        // unordered_map<int,int> m;
-        // for(int i=0;i<nums1.size();i++){
-        //     m[nums1[i]]++;
-        // }
-        // vector<int> ans;
-        // for(int i=0;i<nums2.size();i++){
-        //     int data = nums2[i];
-        //     if(m.find(data) != m.end()){
-        //         ans.push_back(data);
-        //     }
-        // }
-        // unordered_map<int,int> mp;
-        // for(int i=0;i<ans.size();i++){
-        //     mp[ans[i]]++;
-        // }
-        // vector<int> ans1;
-        // for(auto i:mp){
-        //     ans1.push_back(i.first);
-        // }
+
+/*
+        //T.C O(n^2) brute force
         vector<int> ans;
         for(int i = 0;i<nums1.size();i++){
             for(int j = 0;j<nums2.size();j++){
@@ -31,5 +15,25 @@ public:
             }
         }
         return ans;
+    }
+
+*/
+
+    unordered_map<int,int> m;
+    for(int i=0;i<nums1.size();i++){
+        m[nums1[i]]++;
+    }
+    vector<int> ans;
+    for(int i=0;i<nums2.size();i++){
+        int data = nums2[i];
+        if(m.find(data) != m.end()){
+            if(m[data] > 0){
+                ans.push_back(data);
+                m[data]--;
+            }
+        }
+    }
+    return ans;
+
     }
 };
