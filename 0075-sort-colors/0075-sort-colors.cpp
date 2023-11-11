1d593098d2734352d743c9e637a3f467d2c16ecc
Aman Prakash <114851919+AmanPrakash00@users.noreply.github.com>
2023-11-11 18:21:03 +0530

    Time: 0 ms (100%), Space: 8.7 MB (34.1%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,6 +1,43 @@
 class Solution {
 public:
+/* 
+    Question ke according aisa bilkul nhi karna hai
+
     void sortColors(vector<int>& nums) {
         sort(nums.begin(),nums.end());
+    }
+*/
+
+    void sortColors(vector<int>&nums){
+        int n = nums.size();
+        int zero = 0;
+        int one = 0;
+        int two = 0;
+        for(int i = 0;i<n;i++){
+            if(nums[i] == 0){
+                zero++;
+                continue;
+            }
+            if(nums[i] == 1){
+                one++;
+                continue;
+            }
+            two++;
+        }
+
+        for(int i = 0;i<n;i++){
+            if(zero != 0){
+                nums[i] = 0;
+                zero--;
+                continue;
+            }
+            if(one != 0){
+                nums[i] = 1;
+                one--;
+                continue;
+            }
+            nums[i] = 2;
+            two--;
+        }
     }
 };
